7b058b7a57573521aa3bc65df9457b4a5fd76d93
SonalSingh18 <SonalSingh18@users.noreply.github.com>
2020-04-02 04:00:15 +0000

    mido: Enable Dirac audio effect
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -230,3 +230,14 @@ wifi.interface=wlan0
 # Wifi-Display
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.sys.wfd.virtual=0
+
+# Dirac algo tsx 9/12
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dirac.acs.controller=qem \
+    persist.dirac.acs.storeSettings=1 \
+    persist.dirac.acs.ignore_error=1
+
+# Dirac headset effect
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.audio.soundfx.dirac=true \
+    persist.audio.dirac.speaker=true
