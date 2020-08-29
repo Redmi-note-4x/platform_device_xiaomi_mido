beac1f5dbf267c9f0dfd7c33b34bd42809b0a33e
tangrobin <>
2020-08-29 16:43:38 +0530

    mido: import surface flinger offsets from Sunfish
    GPU takes long time(>8ms) when app was launching and closing.
    This may because GPU is handling some heavy task like color conversion.
    By setting NegativeOffset as 5ms to allow SF have more time to handle
    when doing animation transaction and shift back 5ms with
    early_app_phase_offset_ns such that app still has the same amount of
    time to handle each frame.
    
    Test: 1. Open Chrome from homescreen
          2. Swipe vertically to go back to home screen
          3. Measure the gpu completion time and "FrameMissed" count from systrace result
    Bug: 147339695
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -220,7 +220,12 @@ persist.vendor.recovery_update=false
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-ro.surface_flinger.max_virtual_display_dimension=4096
+ro.surface_flinger.max_virtual_display_dimension=4096 \
+debug.sf.early_phase_offset_ns=11600000 \
+debug.sf.early_app_phase_offset_ns=11600000 \
+debug.sf.early_gl_phase_offset_ns=3000000 \
+debug.sf.early_gl_app_phase_offset_ns=15000000 \
+debug.sf.phase_offset_threshold_for_next_vsync_ns=11600000
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
