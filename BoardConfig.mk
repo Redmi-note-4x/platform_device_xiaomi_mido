e151b86cf7befbd8f047e5461d6353eb8ab43775
Julian Fischer <myst3rion9@gmail.com>
2021-01-12 22:57:15 +0100

    mido: power-libperfmgr: Add DT2W based on powerhints
    Co-authored-by: Jesse Chan <jc@lineageos.org>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I11890470f7e11ed7a256851246272a1f11eaf0bc
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,10 +181,6 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 872415232
 # Peripheral manager
 TARGET_PER_MGR_ENABLED := true
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
-TARGET_USES_INTERACTION_BOOST := true
-
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
 
