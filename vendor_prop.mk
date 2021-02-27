d6350ce40a0faf20f44dbf2f8d71a61e2afdac18
Pig <pig.priv@gmail.com>
2021-02-27 12:54:06 +0530

    mido: Move DPM feature property to /system_ext

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -112,7 +112,6 @@ vendor.gralloc.enable_fb_ubwc=1
 
 # DPM
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.dpm.feature=11 \
 persist.vendor.dpm.loglevel=0 \
 persist.vendor.dpm.nsrm.bkg.evt=3955 \
 persist.vendor.dpmhalservice.enable=1
