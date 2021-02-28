f2a41a7094ba2abbb30676ede99288bf79cdd58f
Bruno Martins <bgcngm@gmail.com>
2021-02-28 12:21:52 +0530

    mido: Use generated kernel headers

--- camera/QCamera2/stack/mm-jpeg-interface/Android.mk
+++ camera/QCamera2/stack/mm-jpeg-interface/Android.mk
@@ -1,7 +1,6 @@
 OLD_LOCAL_PATH := $(LOCAL_PATH)
 LOCAL_PATH := $(call my-dir)
 
-include $(LOCAL_PATH)/../../../common.mk
 include $(CLEAR_VARS)
 
 LOCAL_HEADER_LIBRARIES := libutils_headers
@@ -12,18 +11,7 @@ LOCAL_CFLAGS+= -D_ANDROID_ -DQCAMERA_REDEFINE_LOG
 
 LOCAL_CFLAGS += -Wall -Wextra -Werror -Wno-unused-parameter
 
-ifneq ($(TARGET_KERNEL_VERSION),$(filter $(TARGET_KERNEL_VERSION),3.18 4.4 4.9))
-  ifneq ($(LIBION_HEADER_PATH_WRAPPER), )
-    include $(LIBION_HEADER_PATH_WRAPPER)
-    LOCAL_C_INCLUDES += $(LIBION_HEADER_PATHS)
-  else
-    LOCAL_C_INCLUDES += \
-            system/core/libion/kernel-headers \
-            system/core/libion/include
-  endif
-endif
-LOCAL_C_INCLUDES+= $(kernel_includes)
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+LOCAL_HEADER_LIBRARIES += generated_kernel_headers
 
 LIB2D_ROTATION=true
 
