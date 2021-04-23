b6c7ccac2fd4a80a485b440593a4ca2e0303cfc9
Shivan Singh <shivan.0972@gmail.com>
2021-04-24 02:05:19 +0530

    Revert "mido: Add SoundTrigger HAL v2.3 support"
    This reverts commit 4c28f2f7f83d54e7af168c4c292196080571558c.

--- device.mk
+++ device.mk
@@ -89,7 +89,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.soundtrigger@2.3-impl \
+    android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.msm8953 \
