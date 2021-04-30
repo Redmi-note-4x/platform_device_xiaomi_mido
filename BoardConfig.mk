4a0938c2df5a74fbd5b3269c1d74f3abd6d4af37
Kingsman44 <shivan.0972@gmail.com>
2021-04-30 16:50:36 +0530

    mido: ignore never allow
    Only for Torch in XiaomiParts

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,6 +154,7 @@ VENDOR_SECURITY_PATCH := 2021-02-05
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
