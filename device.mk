58487f4d53b33c1190a1ac691400fd7b4bfb8159
Sherif Rahim <sherifrahim2001@gmail.com>
2021-05-09 00:23:43 +0530

    mido: Build MotoDolby v3
    *Port from sofiap_sprout(GPlus) by @reiryuki, (thanks for the helps too..)
    
    files refered here: reiryuki/Moto-Dolby-G-Pro-Audio-FX-Magisk-Module
    reffered commit: HSgod/device_xiaomi_raphael@7708617#diff-7ee7359ba09f5727ba183953197099cff2eacc555c82091a7e3268c87e8e1029
    naveenjohnsonv: removed libeffects, updated APKs from Ryuki's v2.5 GPro module, added correct system props and removed duplicates
    
    Signed-off-by: Sherif Rahim <sherifrahim2001@gmail.com>
    Co-authored-by: SonalSingh18 <sonal.singh.19993@gmail.com>
    Signed-off-by: Kingsman44 <shivan.0972@gmail.com>

--- device.mk
+++ device.mk
@@ -284,7 +284,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
+    $(LOCAL_PATH)/configs/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
