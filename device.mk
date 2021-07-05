31e983c84a42ef0e952b8f9f1d5b3d5c51e54413
weivincewang <wvw@google.com>
2021-07-05 12:05:21 +0530

    mido: Improve boot time and pull in bootanim display time
    1) launch bootanim early
    2) parallel slow init operations: write sysfs
    3) boot runtime fs tune
    
    After CL:
    Boot time saved 330ms, bootanim triggered early before data/ mounted
    
    Bug: 36780513
    Test: walleye boots
    Change-Id: I02803179746710413f4b1e2372f3550cd95d1581
    Signed-off-by: Kingsman44 <shivan.0972@gmail.com>

--- device.mk
+++ device.mk
@@ -258,6 +258,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/msm8953-snd-card-mtp_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/msm8953-snd-card-mtp_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl
 
+# Insmod files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/init.insmod.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/init.insmod.cfg
+
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
@@ -363,7 +367,8 @@ PRODUCT_PACKAGES += \
     init.soundcontrol.rc \
     init.target.rc \
     init.veth_ipa_config.sh \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.insmod.sh \
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
