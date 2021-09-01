4df68d9ce171fd4ed5a23aca2a65c034961b27fa
Zinadin Zidan <tuhinsultana075@gmail.com>
2021-09-01 18:46:46 +0530

    mido: configs: media: Enable media Codec2 * https://source.android.com/devices/media/updatable-media
    Signed-off-by: Zinadin Zidan <tuhinsultana075@gmail.com>
    Signed-off-by: TogoFire <italomellopereira@gmail.com>
    Signed-off-by: YadavMohit19 <mohityadav050575@gmail.com>

--- device.mk
+++ device.mk
@@ -171,6 +171,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Codec2 modules
+PRODUCT_PACKAGES += \
+    com.android.media.swcodec \
+    libsfplugin_ccodec
+
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
