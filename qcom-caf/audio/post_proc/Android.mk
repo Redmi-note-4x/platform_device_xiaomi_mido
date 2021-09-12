a1943828e552ac78e027a47f3af2049097975223
Jabiyeff <cebiyevanar@gmail.com>
2021-09-12 23:29:08 +0400

    mido: hals: Remove project pathmap
    * Adapt paths for device tree
    * Also, adapt camera hal pathmaps
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- qcom-caf/audio/post_proc/Android.mk
+++ qcom-caf/audio/post_proc/Android.mk
@@ -195,16 +195,16 @@ LOCAL_MODULE_OWNER := qti
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_C_INCLUDES := \
-        $(call project-path-for,qcom-audio)/hal \
+        $(LOCAL_PATH)/../hal \
         $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
         $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/audio \
         $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/techpack/audio/include \
         external/tinyalsa/include \
         $(call include-path-for, audio-effects) \
         $(call include-path-for, audio-route) \
-        $(call project-path-for,qcom-audio)/hal/audio_extn \
         external/tinycompress/include \
-        system/media/audio_utils/include
+        system/media/audio_utils/include \
+        $(LOCAL_PATH)/../hal/audio_extn
 
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_DLKM)),true)
   LOCAL_HEADER_LIBRARIES += audio_kernel_headers
