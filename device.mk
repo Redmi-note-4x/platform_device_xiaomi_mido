ab1aa316dca941aa120d9778ef51ffcf398af562
lostark13 <tushar9915@gmail.com>
2021-09-22 13:30:14 +0200

    mido: Switch to AOSP FM Package
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- device.mk
+++ device.mk
@@ -210,9 +210,9 @@ PRODUCT_PACKAGES += \
 
 # FM
 PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio
+    FMRadio \
+    libfmjni \
+    fm-commonsys
 
 # Fwk-detect
 PRODUCT_PACKAGES += \
