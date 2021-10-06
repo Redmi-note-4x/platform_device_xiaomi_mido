28409c55669643b8f6613f04b7e6a101bac6d97c
Rajat Yadav <quic_rajayada@quicinc.com>
2021-10-06 12:28:08 +0530

    commonsys-intf: Smomo support for multi-display
    CRs-Fixed: 3100828
    Change-Id: If86ab632aeb58d583b089d03b57f40f0525f3530

--- qcom-caf/display/display-commonsys-intf/include/composer_extn_intf.h
+++ qcom-caf/display/display-commonsys-intf/include/composer_extn_intf.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2019-2022, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -51,7 +51,7 @@ class ComposerExtnIntf {
   virtual void DestroyDisplayExtn(DisplayExtnIntf *intf) = 0;
   virtual int CreatePhaseOffsetExtn(PhaseOffsetExtnIntf **intf) = 0;
   virtual void DestroyPhaseOffsetExtn(PhaseOffsetExtnIntf *intf) = 0;
-  virtual bool CreateSmomoExtn(smomo::SmomoIntf **intf) = 0;
+  virtual bool CreateSmomoExtn(smomo::SmomoIntf **intf, smomo::DisplayInfo info) = 0;
   virtual void DestroySmomoExtn(smomo::SmomoIntf *intf) = 0;
  protected:
   virtual ~ComposerExtnIntf() { }
