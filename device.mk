a39718140a2caf1ba3a081bcde56eff847191085
Kingsman44 <shivan.0972@gmail.com>
2021-10-15 00:30:24 +0530

    mido: drop QPerformance and UxPerformance

--- device.mk
+++ device.mk
@@ -367,11 +367,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libjson
 
-#QTI performance
-PRODUCT_BOOT_JARS += \
-    QPerformance \
-    UxPerformance
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
