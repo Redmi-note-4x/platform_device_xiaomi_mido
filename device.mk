24b81c5addd1f257a12e24ac2d03ff37fce2b186
Kingsman44 <shivan.0972@gmail.com>
2021-10-25 12:07:35 +0530

    mido: update perf blobs from thyme qssi-user 12 SKQ1.210908.001 21.10.12 release-keys

--- device.mk
+++ device.mk
@@ -348,14 +348,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power-service \
-    android.hardware.power.stats@1.0-service.mock \
-    vendor.qti.hardware.perf@1.0.vendor \
-    vendor.qti.hardware.perf@2.0.vendor \
-    vendor.qti.hardware.perf@2.1.vendor \
-    vendor.qti.hardware.perf@2.2.vendor \
-    vendor.qti.hardware.perf@2.0 \
-    vendor.qti.hardware.perf@2.1 \
-    vendor.qti.hardware.perf@2.2
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
