1ab76b31c8a9325e9d9158f5f0d22c83de0cbfb7
Kingsman44 <shivan.0972@gmail.com>
2021-11-18 12:58:21 +0530

    mido: We are Fluid Again !!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_mido.mk
+    $(LOCAL_DIR)/fluid_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_mido-user \
-    aosp_mido-userdebug \
-    aosp_mido-eng
+    fluid_mido-user \
+    fluid_mido-userdebug \
+    fluid_mido-eng
