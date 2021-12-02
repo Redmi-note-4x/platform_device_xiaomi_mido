f44cd4843fd74cf2e962d8e25c1e614e186e4d22
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-12-02 20:56:24 +0700

    mido: CherishOS Ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fluid_mido.mk
+    $(LOCAL_DIR)/cherish_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    fluid_mido-user \
-    fluid_mido-userdebug \
-    fluid_mido-eng
+    cherish_mido-user \
+    cherish_mido-userdebug \
+    cherish_mido-eng
