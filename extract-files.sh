493094e8ac20abd34a10e3a95621cb440f73a2a5
ZeeLog <Danjel.Zammit@gmail.com>
2021-12-12 10:03:23 +0100

    mido: Update wcnss_service from LA.UM.9.6.3.r1-04400-89xx.0
    * drop no longer needed shim
    * Update sha1sum

--- extract-files.sh
+++ extract-files.sh
@@ -103,8 +103,4 @@ sed -i -e '$a\\    capabilities NET_BIND_SERVICE' "${DEVICE_BLOB_ROOT}"/vendor/e
 # Ims
 "${PATCHELF}" --add-needed "libgui-shim.so" "${DEVICE_BLOB_ROOT}"/system_ext/lib64/lib-imsvideocodec.so
 
-# Wcnss_service - libqmiservices_shim
-"${PATCHELF}" --add-needed "libqmiservices_shim.so" "${DEVICE_BLOB_ROOT}"/vendor/bin/wcnss_service
-sed -i "s|dms_get_service_object_internal_v01|dms_get_service_object_shimshim_v01|g" "${DEVICE_BLOB_ROOT}"/vendor/bin/wcnss_service
-
 "${MY_DIR}/setup-makefiles.sh"
