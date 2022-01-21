f10ec2c84385895b4b6f0a5f2281fcf324c55074
Kingsman44 <shivan.0972@gmail.com>
2022-01-21 21:25:26 +0530

    mido: build android.hardware.power@1.2
    * needed by gnss hal

--- device.mk
+++ device.mk
@@ -361,6 +361,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power-service \
     android.hardware.power.stats@1.0-service.mock \
+    android.hardware.power@1.2 \
+    android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@1.0.vendor \
     vendor.qti.hardware.perf@2.0.vendor \
     vendor.qti.hardware.perf@2.1.vendor \
