20658fbffe0242b1c840f781b0f869c7184fd31d
Kingsman44 <shivan.0972@gmail.com>
2022-02-14 09:36:15 +0530

    Revert "mido: start bootanim before mounting data"
    This reverts commit 361a08c2818af61b3b704c70309aa9217c0eb378.

--- device.mk
+++ device.mk
@@ -281,10 +281,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/msm8953-snd-card-mtp_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/msm8953-snd-card-mtp_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl
 
-# Insmod files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/init.insmod.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/init.insmod.cfg
-
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
@@ -414,8 +410,7 @@ PRODUCT_PACKAGES += \
     init.soundcontrol.rc \
     init.target.rc \
     init.veth_ipa_config.sh \
-    ueventd.qcom.rc \
-    init.insmod.sh
+    ueventd.qcom.rc
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
